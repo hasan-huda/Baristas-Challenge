b45a561f31a7d2f00ae9303c99afd2047d76a39c
Hasan Huda <hasanhuda037@gmail.com>
2023-02-04 13:38:28 -0500

    ready for testing

--- Order.java
+++ Order.java
@@ -14,4 +14,54 @@ public class Order{
     this.name=name;
     items = new ArrayList<Item>();
   }
+
+  public void addItem(Item item){
+    items.add(item);
+  }
+
+  public String getStatusMessage(){
+    String status = ready? "Your order is ready" : "Thank you for waiting. Your order will be ready soon.";
+    return status;
+  }
+
+  public double getOrderTotal(){
+    double sum=0;
+    for(Item item: items){
+      sum+=item.getPrice();
+    }
+    return sum;
+  }
+
+  public void display(){
+    System.out.println("Customer Name: "+name);
+    for(Item item: items){
+      System.out.println(item.getName()+" - $"+item.getPrice());
+    }
+    System.out.println("Total: "+getOrderTotal());
+
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String name){
+    this.name=name;
+  }
+
+  public boolean getReady(){
+    return ready;
+  }
+
+  public void setReady(boolean ready){
+    this.ready=ready;
+  }
+
+  public ArrayList<Item> getItems(){
+    return items;
+  }
+
+  public void setItems(ArrayList<Item> items){
+    this.items=items;
+  }
 }
--- TestOrders.java
+++ TestOrders.java
@@ -3,42 +3,29 @@ public class TestOrders {
     public static void main(String[] args) {
     
         // Menu items
-        Item item1 = new Item();
-        Item item2 = new Item();
-        Item item3 = new Item();
-        Item item4 = new Item();
-        item1.name = "mocha";
-        item1.price = 1.08;
-        item2.name = "latte";
-        item2.price = 2.17;
-        item3.name = "drip coffee";
-        item3.price = 1.62;
-        item4.name = "capuccino";
-        item4.price = 2.55;
+        Item item1 = new Item("mocha",1.08);
+        Item item2 = new Item("latte",2.17);
+        Item item3 = new Item("drip coffee",1.62);
+        Item item4 = new Item("cappuccino",2.55);
         // Order variables -- order1, order2 etc.
         Order order1 = new Order();
         Order order2 = new Order();
-        Order order3 = new Order();
-        Order order4 = new Order();
-        order1.name="Cindhuri";
-        order2.name="Jimmy";
-        order3.name="Noah";
-        order4.name="Sam";
-        order2.items.add(item1);
-        order2.total+=item1.price;
-        order3.items.add(item4);
-        order3.total+=item4.price;
-        order4.items.add(item2);
-        order4.total+=item2.price;
-        order1.ready=true;
-        order4.items.add(item2);
-        order4.items.add(item2);
-        order4.total+=item2.price*2;
-        order2.ready=true;
+        Order order3 =new Order("Bob");
+        Order order4 =new Order("Bobbers");
+        Order order5 =new Order("John");
         // Application Simulations
+        order1.addItem(item1);
+        order1.addItem(item2);
+        order2.addItem(item3);
+        order2.addItem(item4);
+        order3.addItem(item1);
+        order3.addItem(item2);
+        order4.addItem(item3);
+        order4.addItem(item4);
+        order5.addItem(item1);
+        order5.addItem(item2);
+        order1.setReady(true);
+        System.out.println(order1.getStatusMessage());
         // Use this example code to test various orders' updates
-        System.out.printf("Name: %s\n", order1.name);
-        System.out.printf("Total: %s\n", order2.total);
-        System.out.printf("Ready: %s\n", order1.ready);
     }
 }
